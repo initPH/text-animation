6a1c594536bc17dddc49611ad0039efc9cd175a5
ph <penghao@gaoyusoft.com>
2021-08-17 18:34:53 +0800

    随便加了一点

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,12 @@
 <!-- MyBook.vue -->
 <template>
-  <div>{{ collectionName }}: {{ readersNumber }} {{ book.title }}</div>
-  <button @click="readersNumber += 1">1111</button>
+  <pre id="code"></pre>
 </template>
 
-<script>
+<script lang="ts">
 import { ref, reactive } from 'vue';
 import prismjs from 'prismjs';
+import 'prismjs/themes/prism.css';
 export default {
   props: {
     collectionName: String,
@@ -14,13 +14,63 @@ export default {
   setup() {
     const readersNumber = ref(0);
     const book = reactive({ title: 'Vue 3 Guide' });
-    let style = document.createElement('style')
-    style.innerHTML = `
-      html {
-        background: red
+
+    let styleStr = `
+    /*
+    * 面试官你好，我是Edit，广东惠州人
+    * 为您精心准备一份不一样的简历来介绍自己
+    * 首先准备一些样式
+    */
+    *{
+        transition: all .8s;
+    }
+    /* 稍等，在容器上添加点样式 */
+    #codeEdit{
+        color: #fff;
+        background: #1E1E1E;
+    }
+    #resume{
+        box-shadow: -1px 4px 9px 3px rgba(0, 0, 0, .15);
+    }
+    /* 我需要一点代码高亮 */
+    pre#codeEdit{
+        color: #CE9e78;
+    }
+    .token.selector{
+        color: rgb(230, 155, 43);
+    }
+    .token.comment{
+        color: #6A9955;
+        font-size: 14px;
+    }
+    .token.property{
+        color: #60C8FE;
+    }
+    .token.function {
+        color: #DD4A68;
+    }
+    /* 接下来请欣赏我的个人简历吧 */
+`;
+    let num = 0;
+    let styleTag = document.createElement('style');
+    document.head.appendChild(styleTag);
+    let interval = setInterval(() => {
+      if (num < styleStr.length) {
+        document.querySelector('#code').innerHTML = prismjs.highlight(
+          styleStr.slice(0, num),
+          prismjs.languages.css,
+        ) as string;
+        num++;
+
+        styleTag.innerHTML = styleStr.slice(0, num);
+      } else {
+        clearInterval(interval);
       }
-    `
-    document.head.appendChild(style)
+    }, 50);
+
+    let style = document.createElement('style');
+    // document.head.appendChild(style)
+
     // 暴露给 template
     return {
       readersNumber,
